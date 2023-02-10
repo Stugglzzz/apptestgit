a26b277bba8695b14ae0682738cfe6834bd83957
Stugglzzz <Strugglzzz@gmail.com>
2023-02-10 05:55:47 +0100

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/fr/abrsra/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/fr/abrsra/myapplication/ExampleInstrumentedTest.kt
@@ -19,6 +19,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val text = "Text"
         assertEquals("fr.abrsra.myapplication", appContext.packageName)
     }
 }
